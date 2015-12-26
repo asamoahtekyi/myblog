866834229e00a0b9f4527f8f14f6ad0e6efd42dc
samuel tekyi <samuel tekyi>
2015-12-26 08:03:08 +0000

    view change. timezone to datetime

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+#from django.utils import timezone
+from .models import Post
+from datetime import datetime
 
 def post_list(request):
 
-    return render(request,'blog/post_list.html', {})
+    allposts=Post.objects.filter(published_date__lte=datetime.now()).order_by('-published_date')
+         
+    return render(request,'blog/post_list.html', {'allposts':allposts})
+
