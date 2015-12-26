dee22a2a8a0359f340d9b33bc0306417653e7d29
samuel tekyi <samuel tekyi>
2015-12-26 12:07:25 +0000

    the mess

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 #from django.utils import timezone
 from .models import Post
 from datetime import datetime
@@ -8,4 +8,13 @@ def post_list(request):
     allposts=Post.objects.filter(published_date__lte=datetime.now()).order_by('-published_date')
          
     return render(request,'blog/post_list.html', {'allposts':allposts})
+
+
+def post_detail(request, pk):
+
+    allposts = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'blog/post_detail.html', {'allposts':allposts})
+
+   
 
